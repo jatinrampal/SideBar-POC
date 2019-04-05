eecb78b2e97581f4e75cfb08e4dafc7b396fb54f
Jatin Rampal <jatinrampal@hotmail.com>
2019-04-04 21:37:43 -0400

    Initial Commit

---
+++ SideBarPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideBarPOC
+//
+//  Created by Jatin Rampal on 2019-04-04.
+//  Copyright © 2019 Jatin Rampal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
